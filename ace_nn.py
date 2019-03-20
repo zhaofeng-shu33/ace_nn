62a984eb188b56db6a2ba3df6fd1c5db3c564e84
zhaofeng-shu33 <616545598@qq.com>
2019-03-20 19:36:02 +0800

    ace_img: no syntax error and model shape incompatiability

--- ace_nn.py
+++ ace_nn.py
@@ -6,6 +6,7 @@ import keras
 import tensorflow as tf
 from keras.models import Sequential, Model
 from keras.layers import Input, Lambda, Dense
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten
 from sklearn.preprocessing import OneHotEncoder
 from keras import backend as K
 import numpy as np
@@ -23,7 +24,7 @@ def neg_hscore(x):
     cov_f = K.dot(K.transpose(f0), f0) / K.cast(K.shape(f0)[0] - 1, dtype = 'float32')
     cov_g = K.dot(K.transpose(g0), g0) / K.cast(K.shape(g0)[0] - 1, dtype = 'float32')
     return - corr + tf.trace(K.dot(cov_f, cov_g)) / 2
-def ace_nn_img(x, y, ns = 1, epochs = 300, verbose = False, return_hscore = False):
+def ace_nn_img(x, y, ns = 10, epochs = 300, verbose = False, return_hscore = False):
     ''' 
     Uses the alternating conditional expectations algorithm
     to find the transformations of y and x that maximise the 
@@ -50,58 +51,49 @@ def ace_nn_img(x, y, ns = 1, epochs = 300, verbose = False, return_hscore = Fals
     ty : array_like
         the transformed y values.
     '''
-
-    batch_size = x.shape[0]//8
+    
+    batch_size = np.min([x.shape[0]//8, 128])
     hidden_layer_num = 32
     fdim = ns 
     gdim = fdim
-    activation_function = 'tanh'
-    if(len(x.shape) == 1): # if x is rank-1 matrix
-        x_internal = x.reshape([x.shape[0],1])
-    else:
-        x_internal = x
+    activation_function = 'relu'
 
-    if(len(y.shape) == 1): # if y is rank-1 matrix
-        y_internal = y.reshape([y.shape[0],1])
-    else:
-        y_internal = y
-    # implementation note: for categorical labels or 
-    # input data x, one hot encoding is necessary
-    if cat is not None:
-        x_cat = []
-        y_cat = []
-        for i in cat:
-            if (i  < -y_internal.shape[1] or i >= x_internal.shape[1]):
-                raise ValueError("bad cat= specification")
-            if (i < 0):
-                y_cat.append(-i-1)
-            else: # i > 0, for x response variables
-                x_cat.append(i)
-        x_encoder = OneHotEncoder(categorical_features=x_cat)
-        y_encoder = OneHotEncoder(categorical_features=y_cat)
+    x_internal = x.reshape(x.shape[0], x.shape[1], x.shape[2], 1)
+    y_internal = y.reshape(y.shape[0], y.shape[1], y.shape[2], 1)
 
-        x_internal = x_encoder.fit_transform(x_internal)
-        y_internal = y_encoder.fit_transform(y_internal)
+    # channel last image format
+    input_shape_x = (x.shape[1], x.shape[2], 1)
+    input_x = Input(shape = input_shape_x)
+    conv1 = Conv2D(32, kernel_size=(3, 3),
+                     activation=activation_function,
+                     input_shape=input_shape_x)(input_x)
+    conv2 = Conv2D(64, (3, 3), activation=activation_function)(conv1)
+    pool = MaxPooling2D(pool_size=(2, 2))(conv2)    
+    f_internal = Dropout(0.25)(pool)
+    f_internal_2 = Flatten()(f_internal)
+    f = Dense(fdim, activation=activation_function)(f_internal_2)
 
-
-    input_x = Input(shape = (x_internal.shape[1],))
-    f_internal = Dense(hidden_layer_num, activation=activation_function)(input_x)
-    f_internal_2 = Dense(hidden_layer_num, activation=activation_function)(f_internal)
-    f = Dense(fdim)(f_internal_2)
-
-    input_y = Input(shape = (y_internal.shape[1],))
-    g_internal = Dense(hidden_layer_num, activation=activation_function)(input_y)
-    g_internal_2 = Dense(hidden_layer_num, activation=activation_function)(g_internal)
-    g = Dense(gdim)(g_internal_2)
+    input_shape_y = (y.shape[1], y.shape[2], 1)
+    input_y = Input(shape = input_shape_y)
+    conv1_y = Conv2D(32, kernel_size=(3, 3),
+                     activation=activation_function,
+                     input_shape=input_shape_y)(input_y)
+    conv2_y = Conv2D(64, (3, 3), activation=activation_function)(conv1_y)
+    pool_y = MaxPooling2D(pool_size=(2, 2))(conv2_y)    
+    g_internal = Dropout(0.25)(pool_y)
+    g_internal_2 = Flatten()(g_internal)
+    g = Dense(gdim, activation=activation_function)(g_internal_2)
 
     loss = Lambda(neg_hscore)([f, g])
     model = Model(inputs = [input_x, input_y], outputs = loss)
+    # y_pred is loss
     model.compile(optimizer='sgd', loss = lambda y_true,y_pred: y_pred)
     model_f = Model(inputs = input_x, outputs = f)
     model_g = Model(inputs = input_y, outputs = g)
-    model.fit([x_internal, y_internal], np.zeros(x_internal.shape), verbose=verbose,
+    shape_fake = [x_internal.shape[0], x_internal.shape[1], x_internal.shape[2], 1]
+    model.fit([x_internal, y_internal], np.zeros(shape_fake), verbose=verbose,
         batch_size = batch_size, epochs = epochs)
-    h_score = -model.history.history['loss'][-1] # estimation of 1/2 \norm{\widetilde{B}}_F^2
+    h_score = -model.history.history['loss'][-1] # consider taking the average of last five
     t_x = model_f.predict(x_internal)
     t_y = model_g.predict(y_internal)
     if(return_hscore):
